cb6e5ebda2f7edcfb8ad67b968e560a8547142e8
ovplotnikov <106397201+ovplotnikov@users.noreply.github.com>
2023-06-12 16:51:49 +0600

    currentUser Card.js

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,26 +1,44 @@
+import React, { useContext } from "react";
+import CurrentUserContext from "./contexts/CurrentUserContext";
+
 function Card(props) {
+  const currentUser = useContext(CurrentUserContext);
+
+  // Определяем, являемся ли мы владельцем текущей карточки
+  const isOwn = props.card.owner._id === currentUser._id;
+
+  // Определяем, есть ли у карточки лайк, поставленный текущим пользователем
+  const isLiked = props.card.likes.some((i) => i._id === currentUser._id);
+
+  // Создаём переменную, которую после зададим в `className` для кнопки лайка
+  const cardLikeButtonClassName = `elements__like-button ${
+    isLiked ? "elements__like-button_active" : ""
+  }`;
+
   function handleClick() {
     props.onCardClick(props.card);
   }
 
   return (
     <li className="elements__item">
-      <button
-        className="elements__delete-button button-opacity-hover"
-        type="button"
-        aria-label="delete picture"
-      ></button>
+      {isOwn && (
+        <button
+          className="elements__delete-button button-opacity-hover"
+          type="button"
+          aria-label="delete picture"
+        ></button>
+      )}
       <img
         src={props.card.link}
         alt={props.card.name}
         className="elements__image"
-        onClick={handleClick} // Добавлено событие onClick
+        onClick={handleClick}
       />
       <div className="elements__info">
         <h2 className="elements__title">{props.card.name}</h2>
         <div className="elements__like-container">
           <button
-            className="elements__like-button"
+            className={cardLikeButtonClassName}
             type="button"
             aria-label="like button"
           ></button>
